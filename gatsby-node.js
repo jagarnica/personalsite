61c7172870ce3a1070d6016392c8893fc85810fe
Jesus Garnica <gtechy100@gmail.com>
2021-05-28 19:05:58 -0700

    Rename post folders

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,43 +1,5 @@
 const path = require(`path`);
 const { createFilePath } = require(`gatsby-source-filesystem`);
-/**
- * @name getMonth
- * @description Returns the month as a string. Return null if the index is invalid.
- * Starts counting at 0.
- * @param {number} monthNum
- * @return string | null
- */
-const getMonth = function (monthNum) {
-  switch (monthNum) {
-    case 0:
-      return "January";
-    case 1:
-      return "February";
-    case 2:
-      return "March";
-    case 3:
-      return "April";
-    case 4:
-      return "May";
-    case 5:
-      return "June";
-    case 6:
-      return "July";
-    case 7:
-      return "August";
-    case 8:
-      return "September";
-    case 9:
-      return "October";
-    case 10:
-      return "November";
-    case 11:
-      return "December";
-    default:
-      return null;
-  }
-};
-
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
@@ -106,16 +68,9 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       // we will only create that slug if it marked as 'published' or if we are in a development environment
       // Here we actually create the slug value that can be queried by all the nodes.
 
-      const { title, date } = node.frontmatter;
-      // console.log("frontmatter props", title, date);
-      const parsedDate = new Date(date);
-      const year = parsedDate.getFullYear();
-      const month = getMonth(parsedDate.getMonth());
-      const day = parsedDate.getDate();
-
-      const genSlug = encodeURI(`${year}}-${month}}-${day}}_${title}`);
-      //console.log("parsed slug", genSlug);
-      value = `blog` + createFilePath({ node, getNode, basePath: `` });
+      value =
+        `blog` +
+        createFilePath({ node, getNode, basePath: ``, trailingSlash: false });
     }
 
     createNodeField({
