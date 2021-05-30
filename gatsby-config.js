ae8b15fa87ef733936bb23825555e448017fb353
Jesus Garnica <gtechy100@gmail.com>
2021-05-29 19:23:12 -0700

    Fixed minor config typo in gatsby config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -113,7 +113,7 @@ module.exports = {
          * One convention is to place your Netlify CMS customization code in a
          * `src/cms` directory.
          */
-        modulePath: `${__dirname}/src/cms/cms.js`,
+        modulePath: `${__dirname}/src/cms/cms.ts`,
       },
     },
     {
